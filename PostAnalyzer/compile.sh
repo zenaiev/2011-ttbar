ceb5d21936cf202982de2821c7fae033062188a8
nik2305 <you@example.com>
2025-08-05 19:36:10 +0300

    update compile.sh

--- PostAnalyzer/compile.sh
+++ PostAnalyzer/compile.sh
@@ -2,8 +2,10 @@
 
 # compile code (produces two executables)
 #g++ ttbarMakeHist.cxx `root-config --cflags --libs` -lMathMore -o ttbarMakeHist
-g++ -o ttbarMakeHist `root-config --cflags --libs` -lMathMore ttbarMakeHist.cxx fkr/analysisUtils.cc fkr/KinematicReconstruction.cc fkr/KinematicReconstruction_LSroutines.cc fkr/KinematicReconstruction_MeanSol.cc fkr/KinematicReconstructionSolution.cc fkr/sampleHelpers.cc
-g++ ttbarMakePlots.cxx `root-config --cflags --libs` -lMathMore -o ttbarMakePlots
+g++ ttbarMakeHist.cxx fkr/analysisUtils.cc fkr/KinematicReconstruction.cc fkr/KinematicReconstruction_LSroutines.cc fkr/KinematicReconstruction_MeanSol.cc fkr/KinematicReconstructionSolution.cc fkr/sampleHelpers.cc -o ttbarMakeHist `root-config --cflags --libs` -lMathMore  
+g++ ttbarMakePlots.cxx -o ttbarMakePlots `root-config --cflags --libs`
+#g++ -o ttbarMakeHist `root-config --cflags --libs` -lMathMore ttbarMakeHist.cxx fkr/analysisUtils.cc fkr/KinematicReconstruction.cc fkr/KinematicReconstruction_LSroutines.cc fkr/KinematicReconstruction_MeanSol.cc fkr/KinematicReconstructionSolution.cc fkr/sampleHelpers.cc
+#g++ ttbarMakePlots.cxx `root-config --cflags --libs` -lMathMore -o ttbarMakePlots
 
 # create needed directories if do not exist yet
 mkdir -p data mc hist plots
